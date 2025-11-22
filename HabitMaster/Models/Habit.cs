792abc38ca4f6baf505ccaf52b5983f98bcf55a1
Pawel_N <pawel.nowak8@teb.edu.pl>
2025-11-22 16:32:37 +0100

    Zmiany1

---
+++ HabitMaster/Models/Habit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace HabitMaster.Models
+{
+    public class Habit
+    {
+
+        [PrimaryKey, AutoIncrement] 
+        public int Id { get; set; }
+
+        public string Name { get; set; } //nazwa nawyku
+        public string Icon {  get; set; } //nazwa emoji/obrazka użytego obok nawyku (będzie się dało customizować, kiedyś...)
+        
+        public string ColorHex { get; set; } //kolor tła kafelka
+
+        public int CurrentStreak { get; set; } //aktualny streak
+
+        public DateTime LastCompleted { get; set; } //kiedy ostatnio wykonano zadanie
+
+        public bool IsCompletedToday => LastCompleted.Date == DateTime.Today;
+
+    }
+}
